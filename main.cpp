d18d51131e8af2ad343ce183814485b325037640
1wsx10 <1wsx10@gmail.com>
2019-09-13 19:32:20 +1000

    multiple things, check full git log
    moved globals in main into main.h
    moved curse class into main.h
    
    added --onerender arg to only render once

--- main.cpp
+++ main.cpp
@@ -1,22 +1,5 @@
 #include "main.h"
 
-namespace HIT {
-	enum type {
-		undef = 0,
-		sphere = 1,
-	};
-}
-
-typedef struct {
-	v3d c;//centre
-	double r;//radius
-} sphere;
-
-sphere spheres[] = {
-	{v3d(0,0,0), 5},
-	{v3d(-4, 2.5, -3), 1},
-};
-unsigned int num_spheres = sizeof(spheres) / sizeof(spheres[0]);
 
 /** ray_cast to find out if a ray intersects with a sphere
  *
@@ -95,32 +78,11 @@ HIT::type ray_cast(const v3d& start, const v3d& dir,
 	return hit;
 }
 
-class curse {
-	public:
-		struct deleter {
-			void operator()(curse* __ptr) const {
-				endwin();
-			};
-			void end(void *__ptr) const {
-				endwin();
-			};
-		};
 
-		WINDOW *window;
 
-		static std::unique_ptr<curse, curse::deleter> make_unique() {
-			return std::unique_ptr<curse, deleter>(
-					new curse(initscr()), deleter());
-		};
 
-		static std::shared_ptr<curse> make_shared() {
-			return std::shared_ptr<curse>(new curse(initscr()), deleter());
-		};
 
-	protected:
-		// don't want anything to call this
-		curse(WINDOW *window) :window(window) {};
-};
+
 
 std::string get_mouse_name(char *devices_list) {
 
@@ -188,18 +150,9 @@ std::string get_mouse_name(char *devices_list) {
 	}
 }
 
-pthread_mutex_t direction_mutex = PTHREAD_MUTEX_INITIALIZER;
-v3d direction(1,0,0);
-pthread_mutex_t translation_mutex = PTHREAD_MUTEX_INITIALIZER;
-v3d translation(-13, 2, -3);
 
-pthread_mutex_t fov_mutex = PTHREAD_MUTEX_INITIALIZER;
-double fov = 90;
 
-pthread_mutex_t quit_mutex = PTHREAD_MUTEX_INITIALIZER;
-bool quit = false;
 
-const double units_per_sec = 2;
 
 
 void* keys(void *curse_shared_ptr) {
@@ -408,9 +361,25 @@ void* mouse(void *evt_name) {
 }
 
 
+
+
+
 int main(int argc, char **argv) {
 	timer main_timer("main timer");
 
+	bool one_render = false;
+
+	// do args
+	if(argc > 1) {
+		for(int i = 1; i < argc; i++) {
+			if(strncmp(argv[i], arg_onerender, sizeof(arg_onerender)) == 0) {
+				printf("doing one render\n");
+				one_render = true;
+			} else {
+				printf("continuous render\n");
+			}
+		}
+	}
 
 
 	// init ncurses
--- main.h
+++ main.h
@@ -26,3 +26,72 @@
 #define DEBUG 1
 #define IS_APPROX_0(X, DELTA) (abs(X) < (DELTA))
 
+
+pthread_mutex_t direction_mutex = PTHREAD_MUTEX_INITIALIZER;
+v3d direction(1,0,0);
+pthread_mutex_t translation_mutex = PTHREAD_MUTEX_INITIALIZER;
+v3d translation(-13, 2, -3);
+
+pthread_mutex_t fov_mutex = PTHREAD_MUTEX_INITIALIZER;
+double fov = 90;
+
+pthread_mutex_t quit_mutex = PTHREAD_MUTEX_INITIALIZER;
+bool quit = false;
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-const-variable"
+// used in main
+
+const double units_per_sec = 2;
+const char arg_onerender[] = "--one_render";
+
+#pragma GCC diagnostic pop
+
+
+
+
+namespace HIT {
+	enum type {
+		undef = 0,
+		sphere = 1,
+	};
+}
+
+typedef struct {
+	v3d c;//centre
+	double r;//radius
+} sphere;
+
+sphere spheres[] = {
+	{v3d(0,0,0), 5},
+	{v3d(-4, 2.5, -3), 1},
+};
+unsigned int num_spheres = sizeof(spheres) / sizeof(spheres[0]);
+
+
+class curse {
+	public:
+		struct deleter {
+			void operator()(curse* __ptr) const {
+				endwin();
+			};
+			void end(void *__ptr) const {
+				endwin();
+			};
+		};
+
+		WINDOW *window;
+
+		static std::unique_ptr<curse, curse::deleter> make_unique() {
+			return std::unique_ptr<curse, deleter>(
+					new curse(initscr()), deleter());
+		};
+
+		static std::shared_ptr<curse> make_shared() {
+			return std::shared_ptr<curse>(new curse(initscr()), deleter());
+		};
+
+	protected:
+		// don't want anything to call this
+		curse(WINDOW *window) :window(window) {};
+};
